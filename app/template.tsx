f6f44534e7af1172d3b280af684f098ad32b07e4
Zachary Levesque <zlevesque93@gmail.com>
2026-07-13 17:02:45 -0400

    Delete template.tsx

--- app/template.tsx
+++
@@ -1,7 +0,0 @@
-export default function Template({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="page-transition" data-page-transition>
-      {children}
-    </div>
-  );
-}
