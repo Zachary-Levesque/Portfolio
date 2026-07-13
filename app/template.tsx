5fb29988352033a04e2857eb0bade4d0e3710f7a
Zachary Levesque <zlevesque93@gmail.com>
2026-07-13 16:43:22 -0400

    Update template.tsx

--- app/template.tsx
+++ app/template.tsx
@@ -1,3 +1,7 @@
 export default function Template({ children }: { children: React.ReactNode }) {
-  return <div className="page-enter">{children}</div>;
+  return (
+    <div className="page-transition" data-page-transition>
+      {children}
+    </div>
+  );
 }
